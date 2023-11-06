19127db95182fcd89ee2e5275bcc84c40335d91b
LikhitaMagar01 <leekhitame@gmail.com>
2023-11-06 23:09:40 +0545

    feat: register

---
+++ src/app/api/v1/register/route.ts
@@ -0,0 +1,32 @@
+import { db } from "@/lib/db";
+import { NextResponse } from "next/server";
+import * as bcrypt from 'bcrypt';
+import { NextApiRequest, NextApiResponse } from "next";
+
+export async function POST(req: NextApiRequest, res: NextApiResponse) {
+    if(req.method !== 'POST'){
+        return res.status(405).end();
+    }
+    try {
+        const { username, email, password } = await req.body;
+
+        if (!username || !email || !password) {
+            return NextResponse.json({ message: "All fields are required" }, { status: 400 });
+        }
+        const user = await db.user.create({
+            data: {
+                username,
+                email: email.toLowerCase(),
+                password: await bcrypt.hash(password, 10)
+            }
+        })
+
+        const { password: hashedPasswrod, ...result } = user;
+        return NextResponse.json({ result }, { status: 201 });
+
+    }
+    catch (e) {
+        console.error(e);
+        return NextResponse.json({ message: "Something went wrong while trying to register", result: e }, { status: 500 });
+    }
+}
--- src/components/Form/SignInForm.tsx
+++ src/components/Form/SignInForm.tsx
@@ -5,14 +5,9 @@ import { registerUser } from '@/lib/auth'
 
 const SignUpForm: React.FC = () => {
     const router = useRouter()
-    const [username, setUsername] = useState<string>("")
     const [email, setEmail] = useState<string>("")
     const [password, setPassword] = useState<string>("")
     const [error, setError] = useState<string>("")
-
-    const handleNameChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
-        setUsername(e.target.value)
-    }
 
     const handleEmailChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
         setPassword(e.target.value)
@@ -31,22 +26,23 @@ const SignUpForm: React.FC = () => {
     interface RegistrationResponse {
         status: number;
         message: string;
+        // Define other properties as needed
     }
 
     const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
-        try {
-            const res = await registerUser(username, email, password) as RegistrationResponse;
-            if (res.status === 201) {
-                clearInputs();
-                const router = useRouter();
-                router.push('/sign-in');
-            } else {
-                setError(res.message);
-            }
-        } catch (e) {
-            console.error(e);
-        }
+        // try {
+        //     const res = await registerUser(username, email, password) as RegistrationResponse;
+        //     if (res.status === 201) {
+        //         clearInputs();
+        //         const router = useRouter();
+        //         router.push('/login');
+        //     } else {
+        //         setError(res.message);
+        //     }
+        // } catch (e) {
+        //     console.error(e);
+        // }
     };
 
     return (<>
@@ -54,13 +50,7 @@ const SignUpForm: React.FC = () => {
             onSubmit={handleSubmit} className="bg-grey-200 min-h-screen flex flex-col">
             <div className="container max-w-sm mx-auto flex-1 flex flex-col items-center justify-center px-2">
                 <div className="bg-white px-6 py-8 rounded shadow-md text-black w-full">
-                    <h1 className="mb-8 text-3xl text-center">Sign up</h1>
-                    <input
-                        type="text"
-                        className="block border border-grey-light w-full p-3 rounded mb-4"
-                        name="fullname"
-                        placeholder="User Name" />
-
+                    <h1 className="mb-8 text-3xl text-center">Sign In</h1>
                     <input
                         value={email}
                         onChange={handleEmailChange}
@@ -76,31 +66,15 @@ const SignUpForm: React.FC = () => {
                         className="block border border-grey-light w-full p-3 rounded mb-4"
                         name="password"
                         placeholder="Password" />
-                    <input
-                        type="password"
-                        className="block border border-grey-light w-full p-3 rounded mb-4"
-                        name="confirm_password"
-                        placeholder="Confirm Password" />
-
                     <button
                         type="submit"
                         className="w-full text-center py-3 rounded bg-purple-900 text-white hover:bg-purple-900 focus:outline-none focus:bg-purple-900 my-1 cursor-pointer"
-                    >Create Account</button>
-
-                    <div className="text-center text-sm text-grey-dark mt-4">
-                        By signing up, you agree to the
-                        <a className="no-underline border-b border-grey-dark text-grey-dark" href="#">
-                            Terms of Service
-                        </a> and
-                        <a className="no-underline border-b border-grey-dark text-grey-dark" href="#">
-                            Privacy Policy
-                        </a>
-                    </div>
+                    >Log In</button>
                 </div>
-                <div className="text-grey-dark mt-6">
-                    Already have an account?
-                    <a className="no-underline border-b border-blue text-blue" href="/sign-in">
-                        Log in
+                <div className="text-grey-dark mt-6 mr-3">
+                    Don't have an account? Don't Worry 
+                    <a className="ml-2 no-underline border-b border-blue text-blue" href="/sign-up">
+                        Sign Up
                     </a>.
                 </div>
             </div>
