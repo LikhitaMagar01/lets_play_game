7f52b585a01900cb5eb75092df19fdf6d6151dc7
LikhitaMagar01 <leekhitame@gmail.com>
2024-01-16 14:02:19 +0545

    fix: keep userprofile in a component

--- src/app/profile/[id]/page.tsx
+++ src/app/profile/[id]/page.tsx
@@ -1,66 +1,16 @@
-"use client"
-import NavBar from '@/components/NavBar';
-import axios from 'axios';
-import { useRouter } from 'next/navigation';
-import { useEffect, useState } from 'react';
-import toast from 'react-hot-toast';
+"use client";
 
-interface User {
-    id: string;
-    username: string;
-    email: string;
-    // Add other properties as needed
-}
+import UserProfile from "@/components/UserProfile/UserProfile";
 
-const UserProfile: React.FC<{ user: User }> = ({ user }) => {
-    // State to store user details
-    const [userData, setUserData] = useState<User>(user);
-    const router = useRouter()
+const ProfilePage: React.FC = () => {
+  // Example user data (replace this with your actual user fetching logic)
+  const user = {
+    id: "",
+    username: "",
+    email: "",
+  };
 
-    useEffect(() => {
-        const fetchUserDetails = async () => {
-            try {
-                // Fetch user details from /api/me
-                const response = await fetch('/api/v1/me');
-                const data = await response.json();
-
-                if (response.ok) {
-                    // Update the user data in state
-                    setUserData(data.user);
-                } else {
-                    console.error('Error fetching user details:', data.error);
-                }
-            } catch (error) {
-                console.error('Error fetching user details:', error);
-            }
-        };
-
-        fetchUserDetails();
-    }, []); // Run once when the component mounts
-
-    const logOut = () => {
-        try {
-            axios.get(`/api/v1/users/logout`)
-            toast.success("Logout successfully")
-            router.push('/sign-in')
-        } catch (error: any) {
-            console.log(error.message)
-            toast.error(error.message)
-        }
-    }
-    return (
-        <div>
-            <NavBar />
-            <div className='container mx-auto my-8 text-center flex flex-col gap-7 items-center'>
-                <h1 className='underline text-2xl'>User Profile</h1>
-                <p>ID: {userData?.id}</p>
-                <p>Username: {userData?.username}</p>
-                <p>Email: {userData?.email}</p>
-                <button onClick={logOut} className='outline outline-offset-2 outline-1 p-2 cursor-pointer w-fit'>Log Out</button>
-            </div>
-
-        </div>
-    );
+  return <UserProfile user={user} />;
 };
 
-export default UserProfile;
+export default ProfilePage;
